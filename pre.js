9a81fd537d6006ca33e5b585ce2a361011f77ab9
Gonçalo MB <me@goncalomb.com>
2023-04-13 15:30:52 +0100

    Improve exported enums

--- pre.js
+++ pre.js
@@ -1,10 +1,30 @@
 {
+    class EnumSANE {
+        // static reverseObject = Symbol("reverseObject"); // not supported
+
+        static promote(obj) {
+            obj.__proto__ = this.prototype;
+            obj[this.reverseObject] = {};
+            Object.keys(obj).forEach(k => {
+                obj[this.reverseObject][obj[k]] = k;
+            });
+        }
+
+        asString(i) {
+            return this[this.constructor.reverseObject][i] || null;
+        }
+    }
+    EnumSANE.reverseObject = Symbol("reverseObject");
+
     Module.sane = {
         debugSANE: false,
         debugUSB: false,
         ...(Module.sane || {})
     };
+
     Module.preRun = Module.preRun || [];
+    Module.postRun = Module.postRun || [];
+
     Module.preRun.push(() => {
         if (Module.sane.debugSANE) {
             // see ./deps/backends/sanei/sanei_init_debug.c
@@ -19,5 +39,11 @@
             // LIBUSB_LOG_LEVEL_DEBUG = 4
             ENV.LIBUSB_DEBUG = "4";
         }
+    });
+
+    Module.postRun.push(() => {
+        ["SANE_STATUS", "SANE_TYPE", "SANE_UNIT", "SANE_CONSTRAINT", "SANE_FRAME"].forEach(s => {
+            EnumSANE.promote(Module[s]);
+        });
     });
 }
