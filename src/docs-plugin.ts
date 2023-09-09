0c095753852d886981ac71aa2a62dbdf8d3e6b11
Gonçalo MB <me@goncalomb.com>
2023-09-09 21:10:53 +0100

    Fix documentation toolbar links

--- src/docs-plugin.ts
+++ src/docs-plugin.ts
@@ -7,11 +7,30 @@ import { Application, DefaultThemeRenderContext, JSX } from 'typedoc';
  * that would allow the main README.md to be split into multiple files.
  */
 export function load(app: Application) {
-    // remove site menu on index.html
+    // remove site menu and other elements on index.html
     app.renderer.hooks.on('head.end', (context: DefaultThemeRenderContext) => {
         if (context.page.url === 'index.html') {
             return JSX.createElement('style', null, '.site-menu, .tsd-page-title, .tsd-filter-visibility { display: none; }');
         }
         return JSX.createElement(JSX.Fragment, null);
+    });
+
+    // allow relative links on toolbar
+    app.renderer.hooks.on('body.end', (context: DefaultThemeRenderContext) => {
+        return JSX.createElement('script', null, JSX.createElement(JSX.Raw, {
+            html: `
+((els, base) => {
+    els.forEach(el => {
+        const href = el.getAttribute('href');
+        if (href.startsWith('/')) {
+            el.setAttribute('href',\`\${base}\${href}\`);
+        }
+    });
+})(
+    document.querySelectorAll('#tsd-toolbar-links a'),
+    document.getElementById('tsd-search').dataset.base
+);
+`,
+        }));
     });
 }
